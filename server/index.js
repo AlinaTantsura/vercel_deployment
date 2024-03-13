12744ea27b5841e11777e6d691b094f09a5e14a7
Alina <alinatanst@gmail.com>
2024-03-13 10:55:30 +0200

    Added .env

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const app = express();
 
+const{PORT} = process.env
 app.use('/', (req, res) => {
     res.send("Server is running")
 });
 
-app.listen(3001, console.log("Server is running on the port 3001"))
+app.listen(PORT, console.log(`Server is running on the port ${PORT}`))
