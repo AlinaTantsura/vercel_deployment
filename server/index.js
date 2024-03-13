2571bcd4b4a1af6762f6bc7138527809f98a7ab5
Alina <alinatanst@gmail.com>
2024-03-13 11:17:12 +0200

    Added route

--- server/index.js
+++ server/index.js
@@ -6,5 +6,10 @@ const{PORT} = process.env
 app.use('/', (req, res) => {
     res.send("Server is running")
 });
+app.use("/api/", (req, res) => {
+    res.json({
+        message: "This is a route /api/"
+    })
+})
 
 app.listen(PORT, console.log(`Server is running on the port ${PORT}`))
