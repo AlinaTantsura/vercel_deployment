57b6f5d1395b428a807c6320f0df8a044478ff82
Alina <alinatanst@gmail.com>
2024-03-14 10:58:31 +0200

    Test versel

--- server/index.js
+++ server/index.js
@@ -1,32 +1,18 @@
-// const express = require('express');
-// require('dotenv').config()
-// const cors = require('cors')
-// const app = express();
-
-// const { PORT } = process.env
-
-// app.use(cors())
-// app.use('/api/first', (req, res) => {
-//     res.json({ message: "FIRST"})
-// });
-// app.use("/api/second", (req, res) => {
-//     res.json({
-//         message: "SECOND"
-//     })
-// })
-
-// app.listen(PORT, console.log(`Server is running on the port ${PORT}`))
-
 import express from "express";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import "dotenv/config.js";
+// import { medicineRouter } from "./routes/medicineRouter.js";
+// import { orderRouter } from "./routes/orderRouter.js";
 
 const app = express();
+const { DB_HOST, PORT } = process.env;
 
 app.use(cors())
 app.use(express.json());
 
+// app.use("/api/drugshops", medicineRouter);
+// app.use("/api/orders", orderRouter);
 app.use("/api/test", (req, res) => {
     res.json({"message": "This is the test"})
 })
@@ -45,6 +31,13 @@ app.use((err, req, res, next) => {
     res.status(status).json({ message });
 })
 
-app.listen(3001, () => {
-        console.log(`Server is running on port 3001`)
+
+mongoose.connect(DB_HOST).then(() => {
+    console.log("Database connection succesful")
+    app.listen(PORT || 3001, () => {
+        console.log(`Server is running on port ${PORT}`)
     });
+}).catch(err => {
+    console.log(err.message);
+    process.exit(1);
+})
